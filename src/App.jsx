780fbcb3ce9f4ae7d9ffa1a1cb4ed41f1762147e
Luan Masson <luan0masson@gmail.com>
2024-03-08 14:54:52 -0300

    fix - leaving only react logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import reactLogo from "/react.svg";
 import { Button } from "primereact/button";
 
 function handleThemeChange(e) {
@@ -18,9 +17,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
