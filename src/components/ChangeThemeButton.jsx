317e094872192ffe33f384f1a5ca450ef467c829
Luan Masson <111405027+Luan-Masson@users.noreply.github.com>
2025-04-03 18:49:12 -0300

    fix - adicionando muita coisa. Veja descrição
    Adicionado TailWindCSS.
    Criado a lógica de ver o tema do sistema e colocar como padrão.
    Criada lógica de alterar tema.

---
+++ src/components/ChangeThemeButton.jsx
@@ -0,0 +1,40 @@
+import { PrimeReactContext } from "primereact/api"; // Certifique-se de importar corretamente
+import { Button } from "primereact/button"; // Certifique-se de importar corretamente
+import { useContext, useEffect, useState } from "react";
+
+export default function ChangeThemeButton() {
+    const temaEscuro = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const { changeTheme } = useContext(PrimeReactContext);
+    const [currentTheme, setTheme] = useState(() => {
+        return sessionStorage.getItem("theme") || temaEscuro ? "viva-dark" : "viva-light";
+    });
+    
+    useEffect(() => {
+        sessionStorage.setItem("theme", currentTheme);
+    }, [currentTheme]);
+
+    //verifica se o tema salvo foi claro ou escuro e troca o tema do link, evitando o erro de tema escuro ao abrir a página pela primeira vez
+    if (currentTheme === "viva-dark") {
+        const link = document.getElementById("theme").href.replace("viva-light", "viva-dark");
+        document.getElementById("theme").setAttribute("href", link)
+    }
+    
+    const changeThemeFunc = () => {
+        const newTheme = currentTheme === "viva-light" ? "viva-dark" : "viva-light";
+        changeTheme(currentTheme, newTheme, "theme");
+        setTheme(newTheme);
+    };
+
+    return (
+        <Button
+            icon={currentTheme === "viva-dark" ? "pi pi-sun" : "pi pi-moon"}
+            className="p-button-outlined"
+            severity="secondary"
+            rounded
+            text
+            raised
+            onClick={changeThemeFunc}
+            tooltip={currentTheme === "viva-dark" ? "Modo Claro" : "Modo Escuro"} tooltipOptions={{ position: 'left' }}
+        />
+    );
+}
