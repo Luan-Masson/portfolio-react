056c438c4462a44714cab807ffa20b345cf4a5d2
Luan Masson <111405027+Luan-Masson@users.noreply.github.com>
2025-04-09 16:01:56 -0300

    feat - adding a new way to call the backend

--- src/utils/AssetsUtils.js
+++ src/utils/AssetsUtils.js
@@ -10,6 +10,10 @@ export function getLogoUrl(imageName) {
   return import.meta.env.VITE_API_URL + "images/logo/" + imageName;
 }
 
+export function getFromApi(path){
+  return import.meta.env.VITE_API_URL + path;
+}
+
 export function getLanguage(lang) {
   fetch(import.meta.env.VITE_API_URL + "language/" + lang)
     .then((r) => {
